df29b3cb4ea85a445f1bc7863fbf02d46213e9b0
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-03-11 12:13:55 +0300

    add bio tab

---
+++ src/components/Bio.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+tab.bio
+    template(v-slot:info)
+      h3.bio_title BIO.DMG
+    template(v-slot:icon)
+      img.bio_img(:src='account')
+    template(v-slot:content)
+      .bio_content
+        .bio_main 
+          .bio_photo 
+            img.bio_img
+          .bio_info
+        .bio_additional
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Tab from "../components/Tab.vue";
+import Account from "/public/icons/acc.svg";
+
+const account = ref(Account);
+</script>
+
+<style scoped lang="scss">
+.bio {
+  width: 400px;
+  height: 250px;
+  top: 400px;
+  left: 500px;
+
+  &_title {
+    margin: 0;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 100%;
+  }
+
+  &_img {
+    width: 14px;
+    height: 14px;
+    cursor: pointer;
+  }
+
+  &_content {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: 100%;
+  }
+
+  &_main {
+    height: 75%;
+    margin-top: 27px;
+  }
+
+  &_additional {
+    height: 25%;
+    background: repeating-linear-gradient(
+      transparent,transparent 30px, grey 32px
+    ),
+    repeating-linear-gradient(
+      90deg,
+      transparent,transparent 30px, grey 32px
+    );
+    border-top: 1px solid black;
+  }
+}
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+.tab    
+  header.tab_header
+    button.tab_header_options 
+      img.tab_header_img(:src='options')
+    slot(name='info')
+    slot(name='icon') 
+  slot(name='content')
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import Options from '/public/icons/options.svg'
+const options = ref(Options)
+</script>
+
+<style lang="scss">
+.tab {
+    background-color: white;
+    margin: 0;
+    border: 6px solid black;
+    position: relative;
+
+    &_header {
+    width: 100%;
+    padding: 5px 15px;
+    background-color: black;
+    box-sizing:border-box;
+    color: white;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    font-family: 'IBM Plex Mono';
+    font-weight: 400;
+    z-index: 5;
+
+         &_options {
+            padding: 0;
+            background-color: transparent;
+            border: 1px solid transparent;
+            cursor: pointer;
+         }
+
+         &_img {
+            height: 14px;
+            width: 58px;
+         }
+    }
+
+    &::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 10px;
+        background-color: black;
+        bottom: -14px;
+        left: 0;
+    }
+
+    &::before {
+        content: '';
+        position: absolute;
+        width: 10px;
+        height: 100%;
+        background-color: black;
+        bottom: -14px;
+        right: -10px;
+    }
+}
+</style>
